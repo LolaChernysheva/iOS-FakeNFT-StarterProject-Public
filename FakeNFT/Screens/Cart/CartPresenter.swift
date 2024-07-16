804c5a75e60a8ff9ede3b1c7c4f5965f32b21dc5
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-07-16 13:19:54 +0300

    Add build methods, add protocols methods
    Build methods for screen model were implemented
    Added link with a cart presenter and cart view

--- FakeNFT/Screens/Cart/CartPresenter.swift
+++ FakeNFT/Screens/Cart/CartPresenter.swift
@@ -8,19 +8,34 @@
 import Foundation
 
 protocol CartPresenterProtocol: AnyObject {
-    
+    func setup()
 }
 
 final class CartPresenter {
-    weak var view: ProfileViewProtocol?
-    
-    init(view: ProfileViewProtocol?) {
+    weak var view: CartViewProtocol?
+
+    init(view: CartViewProtocol?) {
         self.view = view
+    }
+
+    // MARK: Private Methods
+
+    private func buildScreenModel() -> CartScreenModel {
+        CartScreenModel(
+            items: buildCartItems()
+        )
+    }
+
+    private func buildCartItems() -> [CartItemModel] {
+        let items = Array(repeating: CartItemModel.mock, count: 3)
+        return items
     }
 }
 
 // MARK: CartPresenterProtocol
 
 extension CartPresenter: CartPresenterProtocol {
-    
+    func setup() {
+        view?.update(with: buildScreenModel())
+    }
 }
--- FakeNFT/Screens/Cart/CartViewController.swift
+++ FakeNFT/Screens/Cart/CartViewController.swift
@@ -8,54 +8,56 @@
 import UIKit
 
 protocol CartViewProtocol: AnyObject {
-    
+    func update(with data: CartScreenModel)
 }
 
 final class CartViewController: UIViewController {
-    
+
     // MARK: Public Properties
-    
+
     var presenter: ProfilePresenterProtocol?
-    
+
     // MARK: Private Properties
-    
+
     private lazy var nftTableView: UITableView = {
         let tableView = UITableView()
-        
+
         return tableView
     }()
-    
+
     // MARK: Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         setupViews()
     }
-    
+
     // MARK: Private Methods
-    
+
     private func setupViews() {
         setupPayPanel()
         setupNavBar()
         setupTableView()
     }
-    
+
     private func setupPayPanel() {
-        
+
     }
-    
+
     private func setupNavBar() {
-        
+
     }
-    
+
     private func setupTableView() {
-        
+
     }
 }
 
 // MARK: CartViewProtocol
 
 extension CartViewController: CartViewProtocol {
-    
+    func update(with data: CartScreenModel) {
+
+    }
 }
