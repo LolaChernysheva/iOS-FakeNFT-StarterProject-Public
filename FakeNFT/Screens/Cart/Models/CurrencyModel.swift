fbfa4278019d5d044e6c47948bb9dab2ac540976
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-07-27 19:27:00 +0300

    Add currency collection cell

---
+++ FakeNFT/Screens/Cart/Models/CurrencyModel.swift
@@ -0,0 +1,15 @@
+//
+//  CurrencyModel.swift
+//  FakeNFT
+//
+//  Created by Александр Плешаков on 27.07.2024.
+//
+
+import Foundation
+
+struct CurrencyModel {
+    let title: String
+    let name: String
+    let image: URL?
+    let id: String
+}
